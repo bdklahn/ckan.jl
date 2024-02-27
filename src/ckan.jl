2f42a34a41e89f5d660bd283e337cd0f0a9f563d
Brian D. Klahn <briandk@virginia.edu>
2024-02-27 17:55:26 -0500

    looks like I can pull some json data

--- src/ckan.jl
+++ src/ckan.jl
@@ -1,5 +1,32 @@
 module ckan
 
-# Write your package code here.
+using URIs
+using JSON3
+using HTTP
 
+export url
+
+"""
+Generate a ckan-patterned url
+E.g.
+https://data.virginia.gov/datastore/dump/de3ab3a5-3f98-4254-960b-54bd387e563e?format=json&limit=999999
+"""
+function url(host::String, UUID::String;
+    query::String="limit=999999"
+    )
+    path = "/datastore/dump/$UUID"
+    query = "format=json&$query"
+    URI(;scheme="https", host, path, query)
 end
+
+"""
+Get a JSON dataset from a ckan host.
+"""
+function get(host::String, UUID::String;
+    query::String="limit=999999"
+    )
+    this_url = url(host, UUID; query)
+    JSON3.read(HTTP.get(this_url).body)
+end
+
+end # module ckan
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,11 @@
 using ckan
 using Test
+using URIs
+
+const test_host = "data.virginia.gov"
+const test_UUID = "de3ab3a5-3f98-4254-960b-54bd387e563e"
+const test_query = "limit=3"
 
 @testset "ckan.jl" begin
-    # Write your tests here.
+    @test url(test_host, test_UUID; query=test_query) == URI("https://$test_host/datastore/dump/$test_UUID?format=json&$test_query")
 end
